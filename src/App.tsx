8a3effb232589ad8f1192e83bed3d14755e8f199
Maksim Laurouski <maximlavrovsky@mail.ru>
2022-12-13 18:58:29 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let messageBlockRef = useRef<null | HTMLDivElement>(null)
+
+    const [message, setMessage] = useState('')
+    let [ws, setWs] = useState<WebSocket | null>(null)
+    const [messagesWS, setMessagesWS] = useState<any>([])
+    if (ws) {
+        ws.onmessage = (message) => {
+            let messages = JSON.parse(message.data)
+            setMessagesWS([...messagesWS, ...messages])
+        }
+    }
+
+    useEffect(()=>{
+        messageBlockRef.current?.scrollIntoView()
+    }, [messagesWS])
+
+    useEffect(() => {
+        let localWS = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+        setWs(localWS)
+    }, [])
+
+    function sendMessage() {
+        ws && ws.send(message)
+        setMessage('')
+    }
+
+    return (
+        <div className="App">
+            <div className={'chat'}>
+                <div className={'messages'}>
+                    <div className={'message'}>
+                        {
+                            messagesWS.map((m: any, index: number) => <div className={'message'} ref={messageBlockRef}
+                                                                           key={Math.random() + index}>
+                                <img src={m.photo}/><b>{m.userName}</b>
+                                <span>{m.message}</span></div>)
+                        }
+                    </div>
+                </div>
+                <div className={'footer'}>
+                    <textarea onChange={e => setMessage(e.currentTarget.value)} value={message}/>
+                    <button onClick={sendMessage}>Send</button>
+                </div>
+            </div>
+        </div>);
 }
 
 export default App;
